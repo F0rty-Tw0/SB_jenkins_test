e0cb595153066a8899943c6fc3f708fb1721ba60
joneikholm <joneikholm@gmail.com>
2019-05-13 12:55:33 +0200

    interns first commit

--- src/test/java/com/example/helloworld/HomeControllerTest.java
+++ src/test/java/com/example/helloworld/HomeControllerTest.java
@@ -1,16 +1,16 @@
 package com.example.helloworld;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.Assert.*;
 
 public class HomeControllerTest {
 
+    // her tester vi udenfor Spring context:
     private HomeController homeController = new HomeController();
 
     @Test
     public void home() {
-        assertEquals("index", homeController.home());
+         assertEquals("index", homeController.home());
     }
 }
