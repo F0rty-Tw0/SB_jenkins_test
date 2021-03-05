bab9e7f33fee58fb0ccecc352f3091abcd31b1f9
Jon Eikholm <jone@kea.dk>
2021-03-05 15:06:56 +0100

    ok

--- src/main/java/com/example/helloworld/HelloworldApplication.java
+++ src/main/java/com/example/helloworld/HelloworldApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class HelloworldApplication {
 
     public static void main(String[] args) {
+//        System.out.println(System.getProperty("java.version"));
         SpringApplication.run(HelloworldApplication.class, args);
     }
 
